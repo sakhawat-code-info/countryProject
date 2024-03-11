4729b7bb857cc56bffe7e87f5f0920c7dbc94d60
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-03-11 16:21:15 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# React + Vite
+
+country project
+
+1. App jsx holo main file jai khane final production deya hoy
+component gulo show korar jonno main terminal holo App.jsx
+2. component folder creat kora hoise sob component k alada korar jonno css thakle folder kore raping korar jonno component (folder one, forder two) emn kore component gulo thakbe
+
+3. jar jar component e tar tar data load korte hobe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+
+
+
+const Countries = () => {
+
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, []);
+
+    return (
+        <div>
+            <h2>All Countries {countries.length}</h2>
+
+
+
+
+
+        </div>
+    );
+};
+
+export default Countries;
